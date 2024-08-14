34d1ca42fcfc6b390aa9fbd66fd0e87e4235bcbc
Hamzaba92 <153004965+Hamzaba92@users.noreply.github.com>
2024-08-14 08:36:00 +0200

    1

---
+++ todolist/views.py
@@ -0,0 +1,13 @@
+
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+
+
+
+def hello_world(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+
+
